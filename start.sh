a8dfcbc41ee5e7d540acad9b8821c1f2903384d9
tgvc <84737283+tgvc@users.noreply.github.com>
2021-08-24 15:10:50 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@ echo "Cloning Repo, Please Wait..."
 git clone https://github.com/tgvc/RadioPlayerV3.git /RadioPlayerV3
 cd /RadioPlayerV3
 pip3 install -U -r requirements.txt
+RUN pip3 install flask
+RUN pip3 install flask_restful
 echo "Starting Bot, Please Wait..."
 python3 clever.py &
 python3 main.py

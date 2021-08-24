0c18b5daf1df773234a289e3cf41b1f35974078f
tgvc <84737283+tgvc@users.noreply.github.com>
2021-08-24 15:06:32 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ git clone https://github.com/AsmSafone/RadioPlayerV3.git /RadioPlayerV3
 cd /RadioPlayerV3
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
+python3 clever.py &
 python3 main.py
